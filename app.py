691934cd166b43de7336ba091fcf4e353adcfc4d
Domingo Benavidez <sqxdc@iup.edu>
2025-12-06 14:47:30 -0500

    security fix lol

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import random
+import os
+from dotenv import load_dotenv
 
-# Spotify API credentials - 
-CLIENT_ID = 'f08303c2ccff4d9d9da856a895ad4eaf'
-CLIENT_SECRET = '9e5dc694788648669cb7cefad428b0cc'
+# Load environment variables from .env file
+load_dotenv()
+
+# Spotify API credentials from environment variables
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 # Mood to audio features mapping
 MOOD_FEATURES = {
